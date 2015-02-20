6262d3a3ece320bac51793ed6c54b3840de9f7bb
Will Jones <willjones@ua.edu>
2015-02-20 10:25:13 -0600

    grunt updates, added bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,11 +28,40 @@ module.exports = function(grunt){
                 src: ['src/**/*.css'],
                 dest: 'dist/onesearch.css'
             }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist',
+                    src: '**/*.js',
+                    dest: 'dist',
+                    ext: '-<%= pkg.version %>.min.js'
+                }]
+            }
+        },
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'dist',
+                src: ['*.css', '!*.min.css'],
+                dest: 'dist',
+                ext: '-<%= pkg.version %>.min.css'
+            }
+        },
+        clean: {
+            js: ['dist']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('default', ['html2js', 'concat']);
 };
