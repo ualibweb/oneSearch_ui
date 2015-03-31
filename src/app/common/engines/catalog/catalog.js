49f87fc340c4595d0f5f9c9f82cf44f534b7dde3
bvkrylov <bvkrylov@ua.edu>
2015-03-31 15:16:44 -0500

    Media types fix for Catalog

--- src/app/common/engines/catalog/catalog.js
+++ src/app/common/engines/catalog/catalog.js
@@ -10,7 +10,7 @@ angular.module('engines.catalog', [])
                 types: {
                     books: ['aa','ac', 'ad', 'am'],
                     journals: ['ab','as','bb','bs','cb','cs','db','ds','eb','es','fb','fs','gb','gs','ib','is','jb','js','kb','ks','mb','ms','ob','os','pb','ps','rb','rs','tb','ts'],
-                    media: ['ca','cc','cd','cm','da','dc','dd','dm','ga','gc','gd','gm','ia','ic','id','im','ja','jc','jd','jm','ka','kc','kd','km','oa','oc','od','om','ra','rc','rd','rm']
+                    media: ['ga','gc','gd','gm','ia','ic','id','im','ja','jc','jd','jm']
                 }
             },
             templateUrl: 'common/engines/catalog/catalog.tpl.html'
