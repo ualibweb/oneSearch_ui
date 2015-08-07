34f1b5000e91bda09411bd4289c1fca138a1fdb2
bvkrylov <bvkrylov@ua.edu>
2015-08-07 11:27:27 -0500

    Suggest directive fix try6

--- src/app/common/directives/suggest/suggest.js
+++ src/app/common/directives/suggest/suggest.js
@@ -169,7 +169,11 @@ angular.module('oneSearch.common')
                 scope.handleSelection = function(selectedItem) {
                     $timeout(function() {
                         scope.model = selectedItem;
-                        scope.selected = false;
+                        scope.originalValue = "";
+                        $scope.items = {};
+                        $scope.setCurrent(-1, false);
+                        $scope.dataRequested = false;
+                        $scope.selected = false;
                         scope.$apply();
                         scope.search();
                     }, 0);
