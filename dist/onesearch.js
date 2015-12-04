58ab66b4e85cdd4fd18e84fcd876b8b8da1144e5
Will Jones <the8bitsquid@gmail.com>
2015-12-04 16:22:37 -0600

    fixed Scout 'more link' facet tokenization to work with EBSCO test solution

--- dist/onesearch.js
+++ dist/onesearch.js
@@ -1367,12 +1367,16 @@ angular.module('engines.scout', [])
         var link = angular.copy($scope.resourceLink);
 
         // Tokenize box name to camelCase for EDS inject script
-        box = box.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+        /*box = box.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
             if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
             return index == 0 ? match.toLowerCase() : match.toUpperCase();
-        });
+        });*/
+        if (box === 'Articles'){
+            box = 'AcademicJournals';
+        }
 
         if (link.indexOf('facet=') > 0){
+
             link = link.replace(/&facet=(.+)&?/, box);
         }
         else {
