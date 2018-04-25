c6f5f3d0d6c70fb271b1916512e070e920a34016
amparker8 <andrew-parker-1@uiowa.edu>
2018-04-25 13:35:37 -0500

    Dev build: Change LibAnswers back to Google API

--- src/app/common/engines/faq/faq.js
+++ src/app/common/engines/faq/faq.js
@@ -23,10 +23,11 @@ angular.module('engines.faq', [])
      */
     .config(['oneSearchProvider', function(oneSearchProvider){
         oneSearchProvider.engine('faq', {
-            id: 1024,
-            title: 'LibAnswers',
+            id: 16,
             priority: 2,
-            resultsPath: 'LibAnswers.query.results',
-            templateUrl: 'common/engines/faq/faq.tpl.html'
+            resultsPath: 'GoogleCS.items',
+            totalsPath: 'GoogleCS.searchInformation.totalResults',
+            filterQuery: 'site:ask.lib.ua.edu',
+            templateUrl: 'common/engines/google-cs/google-cs.tpl.html'
         })
     }])
